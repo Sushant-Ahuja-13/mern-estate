caa4e05d3aff8ac56443940de3180a395d9cce43
Sushant Ahuja <12212008@nitkkr.ac.in>
2025-04-11 12:16:34 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10); // 10 is the salt rounds
@@ -10,6 +11,20 @@ export const signup = async (req, res, next) => {
         res.status(201).json({ message: 'User created successfully' });
     } catch (err) {
         // next(errorHandler(550, 'Error from the function'))
+        next(err);
+    }
+};
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        const { password: pass, ...rest } = validUser._doc;
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+    } catch (err) {
         next(err);
     }
 }
